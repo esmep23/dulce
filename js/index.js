856edfa2f62ddecd9d74a36ea6e65d8d4e693d1f
esmep23 <stefipulgar@gmail.com>
2015-09-02 16:32:23 -0500

    '3erdia-10FINAL2'

--- js/index.js
+++ js/index.js
@@ -27,70 +27,21 @@ var mesNoticia;
 /////////////////////////////////////////////////////////////
 partidoActual = 8;
 
-*/
+
 /////////////////////////////////////////////////////////
 $( document ).ready(function() {
 
 
   var tamanPantallaHeight = $(window).height()+100;
   porcentaje = (tamanPantallaHeight * 66) /100;
-  $('#video').css('width','100%');
-  $('#video').css('height','100%');
+  $('#video').css('width',$(window).width());
+  $('#video').css('height',tamanPantallaHeight-100);
 
   $('#maximun').css('height',tamanPantallaHeight-100);
   $('#mypanel .ui-panel-inner').css('height', tamanPantallaHeight);
   $('#mypanel ul').css('height', tamanPantallaHeight-200);
 
-  bgadj();
-
- function bgadj(){
-         
-        var videoActualWidth = video.getBoundingClientRect().width;
-        var videoActualHeight = video.getBoundingClientRect().height;
-              
-        var ratio =  videoActualWidth / videoActualHeight;         
-         
-        if ((window.innerWidth / window.innerHeight) < ratio){
-          
-            video.setAttribute("style", "width: auto");
-            video.setAttribute("style", "height: 100%");
-              
-            <!-- si el vídeo es mas ancho que la ventana lo centro. Esta parte es opcional -->
-            if (videoActualWidth > window.innerWidth){
-              
-                var ajuste = (window.innerWidth - videoActualWidth)/2;                
-                
-                video.setAttribute("style", "left:"+ajuste+"px");          
-            }
-          
-        }
-        else{ 
-          
-            video.setAttribute("style", "width: 100%");
-            video.setAttribute("style", "height: auto");
-            video.setAttribute("style", "left: 0");
-  
-        }
-          
-    }
  
-/*$('#inicio > div').videoBG({
-        mp4:'http://barcelonasc.com.ec/apps/bsc_news/video/INTRO.mp4',
-        ogv:'http://barcelonasc.com.ec/apps/bsc_news/video/INTRO.ogv',
-        webm:'http://barcelonasc.com.ec/apps/bsc_news/video/INTRO.webm',
-        poster:'http://barcelonasc.com.ec/apps/bsc_news/video/INTRO.jpg',
-        autoplay:true,
-        loop:true,
-        scale:false,
-        position:"absolute",
-        opacity:1,
-        textReplacement:false,
-        zIndex:1,
-        width:0,
-        height:0,
-        fullscreen:true,
-        imgFallback:false
-    });*/
 
 
   var value = localStorage.getItem('token');
@@ -205,8 +156,8 @@ $( document ).ready(function() {
 
 
 
-/*});*/
-}
+});
+
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 function descargo(argument){
   //alert(argument);

4542673ae1de1bc4aea1545f6520755cadd9d8fa
esmep23 <stefipulgar@gmail.com>
2015-09-02 12:08:55 -0500

    '3erdia-6'

--- js/index.js
+++ js/index.js
@@ -32,10 +32,50 @@ function onDeviceReady() {
     navigator.splashscreen.show();
 }*/
 
+window.onload = function() {
+  
+    function bgadj(){
+         
+        var videoActualWidth = video.getBoundingClientRect().width;
+        var videoActualHeight = video.getBoundingClientRect().height;
+              
+        var ratio =  videoActualWidth / videoActualHeight;         
+         
+        if ((window.innerWidth / window.innerHeight) < ratio){
+          
+            video.setAttribute("style", "width: auto");
+            video.setAttribute("style", "height: 100%");
+              
+            <!-- si el vídeo es mas ancho que la ventana lo centro. Esta parte es opcional -->
+            if (videoActualWidth > window.innerWidth){
+              
+                var ajuste = (window.innerWidth - videoActualWidth)/2;                
+                
+                video.setAttribute("style", "left:"+ajuste+"px");          
+            }
+          
+        }
+        else{ 
+          
+            video.setAttribute("style", "width: 100%");
+            video.setAttribute("style", "height: auto");
+            video.setAttribute("style", "left: 0");
+  
+        }
+          
+    }
+ 
+    <!-- vuelvo a llamar a la función  bgadj() al redimensionar la ventana -->
+    window.onresize = function() {
+        bgadj();
+  
+    }
+  
+}
  function onLoad() {
   alert(1);
     //navigator.splashscreen.show();
-    $('#inicio > div').videoBG({
+    /*$('#inicio > div').videoBG({
         mp4:'http://barcelonasc.com.ec/apps/bsc_news/video/INTRO.mp4',
         ogv:'http://barcelonasc.com.ec/apps/bsc_news/video/INTRO.ogv',
         webm:'http://barcelonasc.com.ec/apps/bsc_news/video/INTRO.webm',
@@ -51,7 +91,7 @@ function onDeviceReady() {
         height:0,
         fullscreen:true,
         imgFallback:false
-    });
+    });*/
     document.addEventListener("deviceready", onDeviceReady, false);
     
 }

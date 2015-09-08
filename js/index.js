2743eccfcf01600622aed3fc8f4fef89feed727e
esmep23 <stefipulgar@gmail.com>
2015-09-08 17:20:43 -0500

    pruebo error15

--- js/index.js
+++ js/index.js
@@ -408,9 +408,9 @@ function getVideos() {
       success: function(response){  
         if(response!=null && response!='' && response!='[]'){ 
           $.each(response,function(key,value){ 
-            link = value.link;
-            console.log(link);
-            $('.sliderVideo').append('<div style="padding-left:10%"><object width="100%"><param name="movie" value="https://www.youtube.com/v/'+link+'&hl=en_US&feature=player_embedded&version=3"></param><param name="allowFullScreen" value="true"></param><param name="allowScriptAccess" value="always"></param><embed src="https://www.youtube.com/v/'+link+'?suggestedQuality=medium&hl=en_US&feature=player_embedded&version=3" type="application/x-shockwave-flash" allowfullscreen="true" allowScriptAccess="always" width="100%"></embed></object></div>');
+            video_embeded = value.link;
+            console.log(video_embeded);
+            $('.sliderVideo').append('<div><object><param name="movie" value="https://www.youtube.com/v/'+video_embeded+'&hl=en_US&feature=player_embedded&version=3"></param><param name="allowFullScreen" value="true"></param><param name="allowScriptAccess" value="always"></param><embed src="https://www.youtube.com/v/'+video_embeded+'?suggestedQuality=medium&hl=en_US&feature=player_embedded&version=3&theme=dark" width="320" height="240" type="application/x-shockwave-flash" allowfullscreen="true" allowScriptAccess="always"></embed></object></div>');
             //$('.sliderVideo').append('<div><iframe id="bsc'+key+'" width="100%" height="150" src="https://www.youtube.com/embed/'+link+'?rel=0&amp;controls=1&amp;showinfo=0" frameborder="0" allowfullscreen></iframe></div>');
           });
         }
